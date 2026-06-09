94c6a4015ecd8e089c92b452d8f3ddb480227492
Mark Shirley <mark.h.shirley@gmail.com>
2026-06-08 17:59:38 -0700

    Fix tensor_ops lowering for AD; update property tests

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -397,6 +397,7 @@ class TestCompiledVsInterpreter:
         "(iscan/zero + 0 [2 10 5])",
         "(escan/zero + 0 [2 10 5])",
         "(iscan + [0 0 0] [[1 2 3] [4 5 6] [7 8 9]])",
+        "(iscan + [[0 0] [0 0]] [[[1 2] [3 4]] [[5 6] [7 8]] [[9 10] [11 12]]])",
     ])
     def test_scan_family(self, src):
         _assert_lisp_compiled_matches_interp(src)
